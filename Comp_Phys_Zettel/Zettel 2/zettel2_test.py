675aac6be8a65bd30d9a06b8e96af7bff626a30f
nlwach <56191503+nlwach@users.noreply.github.com>
2020-05-01 21:05:15 +0200

    Jupyter Notebook aufgabe 1 halb

--- Comp_Phys_Zettel/Zettel 2/zettel2_test.py
+++ Comp_Phys_Zettel/Zettel 2/zettel2_test.py
@@ -4,92 +4,63 @@ import numpy as np
 from numpy.linalg import norm
 
 
-def s_i(vector_s_i_1, vector_w_i_1, h):
-    return vector_s_i_1 + vector_w_i_1*h
-
-def w_i(vector_s_i_1 ,vector_w_i_1, h):
-    return vector_w_i_1 - vector_s_i_1/norm(vector_s_i_1)**3*h
-
-
-def calc(vector_s_0, vector_w_0, h, steps):
-    for step in range(steps):
-        W.append(vector_w_0)
-        S.append(vector_s_0)
-        e = energy(vector_w_0, vector_s_0)
-        E.append(e)
-        value_vector_s_0 = s_i(vector_s_0, vector_w_0, h)
-        vector_w_0 = w_i(vector_s_0, vector_w_0, h)
-        vector_s_0 = value_vector_s_0
-
 def energy(w, s):
     return norm(w)**2/2 - 1/norm(s)
 
-v = np.array([0, 1])
-s = np.array([1, 0])
+def energy_error(Energies, E_0):
+    return np.abs((Energies-E_0)/E_0)
 
-S = []
-W = []
-E = []
 
-e_1 = energy(v, s)
-w = w_i(s, v, 0.1)
-s = s_i(s, v, 0.1)
-e_2 = energy(w, s)
-#print(s)
-#print(w)
-#print(e_1)
-#print(e_2)
-#calc(s, v, 100, 100)
-print(S)
-print(W)
-print(E)
 
-x_s = []
-y_s = []
-z_s = []
-x_w = []
-y_w = []
-z_w = []
-for i in S:
-    x_s.append(i[0])
-    y_s.append(i[1])
-    z_s.append(i[2])
-for i in W:
-    x_w.append(i[0])
-    y_w.append(i[1])
-    z_w.append(i[2])
-
-#ax.scatter(x_w, y_w, z_w)
-
-#plt.show()
+def turn_vectors_into_2_arrays(List):
+    x_list = []
+    y_list = []
+    for i in List:
+        x_list.append(i[0])
+        y_list.append(i[1])
+    return x_list, y_list
 
 def calculate(s_0, w_0, h, steps):
+    S = []
+    W = []
+    E = []
     for i in range(steps):
-        s_1 = s_0 + w_0*h
-        w_1 = w_0 - (s_0/norm(s_0)**3) * h
-        W.append(w_1)
-        S.append(s_1)
-        e = energy(w_1, s_1)
+        s_i = s_0 + w_0*h
+        w_i = w_0 - (s_0/norm(s_0)**3) * h
+        W.append(w_i)
+        S.append(s_i)
+        e = energy(w_i, s_i)
         E.append(e)
-        s_0 = s_1
-        w_0 = w_1
+        s_0 = s_i
+        w_0 = w_i
+    x_s, y_s = turn_vectors_into_2_arrays(S)
+    return x_s, y_s, E
 
-calculate(s, v, 0.01, 670)
-print(S)
-print(W)
-print(E)
-for i in S:
-    x_s.append(i[0])
-    y_s.append(i[1])
-for i in W:
-    x_w.append(i[0])
-    y_w.append(i[1])
-fig = plt.figure()
+w = np.array([0, 1])
+s = np.array([1, 0])
 
-#plt.axis('scaled')
+steps = 670
+
+w = np.array([0, 0.6])
+s = np.array([1, 0])
+steps = 200
+stepsize = 0.01
+
+plt.figure(figsize=(11,8))
+
+x_s, y_s, E = calculate(s, w, stepsize, steps)
+rel_E_err = energy_error(E, energy(w,s))
+delta_t = np.linspace(0.001, 1)
 zero = np.zeros(len(x_s))
-plt.xlim(-3, 3)
-plt.ylim(-3, 3)
-plt.quiver(zero, zero, x_s, y_s, scale=1, angles="xy", scale_units="xy")
+plt.xlim(-1.5, 1.5)
+plt.ylim(-1.5, 1.5)
+#plt.quiver(zero, zero, x_s, y_s, scale=1, angles="xy", scale_units="xy")
+plt.scatter(x_s, y_s, c="gray", marker=".")
 plt.gca().add_artist(plt.Circle((0, 0), 1, fill=False, color="r"))
+#plt.show()
+steps_array = np.linspace(0, steps, steps)
+plt.figure(figsize=(11,8))
+plt.plot(steps_array, rel_E_err)
+
+
 plt.show()
